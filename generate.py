49cc21ea5c579a6a519d1c919618af4b85cb66af
bstorm <bstorm@wikimedia.org>
2020-03-17 13:25:22 -0700

    Switch to black formatting.
    Change-Id: I2a623b7cb99c2550ec4e2590d86764e4aba40485

--- generate.py
+++ generate.py
@@ -17,29 +17,34 @@ import requests
 def github_stars(user, repo, token):
     """Get number of github stars a repo has"""
     url = "https://api.github.com/repos/%s/%s" % (user, repo)
-    headers = {'Authorization': 'token {}'.format(token)}
+    headers = {"Authorization": "token {}".format(token)}
     data = requests.get(url, headers=headers).json()
-    return data.get('stargazers_count', 0)
+    return data.get("stargazers_count", 0)
 
 
 def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
-        '--token', dest='github_token', metavar='<github_token>', type=str,
+        "--token",
+        dest="github_token",
+        metavar="<github_token>",
+        type=str,
         required=True,
         help=(
-            'A filename with a GitHub personal access token'
-            'with public_repos permission'))
-    argparser.add_argument('outputpath', help='Path to html output')
+            "A filename with a GitHub personal access token"
+            "with public_repos permission"
+        ),
+    )
+    argparser.add_argument("outputpath", help="Path to html output")
 
     args = argparser.parse_args()
 
     outputdir = args.outputpath
 
-    with open('template.html.j2') as index_temp:
+    with open("template.html.j2") as index_temp:
         html = jinja2.Template(index_temp.read(), autoescape=True)
 
-    with open('modal-template.html.j2') as modal_temp:
+    with open("modal-template.html.j2") as modal_temp:
         helper = jinja2.Template(modal_temp.read(), autoescape=True)
 
     with open(args.github_token) as token_file:
@@ -53,48 +58,55 @@ def main():
     with requests.get(list_url, stream=True) as resp:
         json_resp = resp.json()
 
-    all_packages = json_resp['results']
+    all_packages = json_resp["results"]
 
     libraries = []
     for package in all_packages:
         lib = {
-            'name': package['name'],
-            'description': package.get('description', None),
-            'version': package.get('version', None),
-            'homepage': package.get('homepage', None),
-            'keywords': package.get('keywords', []),
-            'assets': package.get('assets', [])
+            "name": package["name"],
+            "description": package.get("description", None),
+            "version": package.get("version", None),
+            "homepage": package.get("homepage", None),
+            "keywords": package.get("keywords", []),
+            "assets": package.get("assets", []),
         }
-        assets_url = upstream_url + "/" + urllib.parse.quote(lib['name']) + "?fields=assets"
+        assets_url = (
+            upstream_url
+            + "/"
+            + urllib.parse.quote(lib["name"])
+            + "?fields=assets"
+        )
         with requests.get(assets_url) as resp:
-            lib['assets'] = resp.json()['assets']
-        if package['repository'] and \
-                'github.com/' in package['repository'].get('url', ''):
-            url = package['repository']['url']
+            lib["assets"] = resp.json()["assets"]
+        if package["repository"] and "github.com/" in package["repository"].get(
+            "url", ""
+        ):
+            url = package["repository"]["url"]
         else:
             url = None
 
         if url is not None:
-            parts = re.sub(r'^\w+://', '', url).split('/')
+            parts = re.sub(r"^\w+://", "", url).split("/")
             user_name, repo_name = parts[-2], parts[-1]
-            if repo_name.endswith('.git'):
+            if repo_name.endswith(".git"):
                 repo_name = repo_name[:-4]
             # print('Fetching starcount for {}/{}'.format(user_name, repo_name))
-            lib['stars'] = github_stars(user_name, repo_name, github_token)
+            lib["stars"] = github_stars(user_name, repo_name, github_token)
         libraries.append(lib)
 
-    libraries.sort(key=lambda lib: lib.get('stars', 0), reverse=True)
-    with open(os.path.join(outputdir, 'index.html'), 'w', encoding='utf8') as index_file:
-        index_file.write(html.render({
-            'libraries': libraries,
-        }))
+    libraries.sort(key=lambda lib: lib.get("stars", 0), reverse=True)
+    with open(
+        os.path.join(outputdir, "index.html"), "w", encoding="utf8"
+    ) as index_file:
+        index_file.write(html.render({"libraries": libraries}))
     for lib in libraries:
-        with open(os.path.join(outputdir, 'mod' + lib['name'] + '.html'),
-                  'w', encoding='utf8') as modal_file:
-            modal_file.write(
-                helper.render({'lib': lib})
-            )
+        with open(
+            os.path.join(outputdir, "mod" + lib["name"] + ".html"),
+            "w",
+            encoding="utf8",
+        ) as modal_file:
+            modal_file.write(helper.render({"lib": lib}))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
